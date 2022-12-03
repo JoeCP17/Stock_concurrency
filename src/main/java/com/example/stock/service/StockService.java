beacdc417d6ee32554c5032307f0ec610263a918
kimueibin <at365@naver.com>
2022-12-04 01:41:05 +0900

    update

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -3,6 +3,7 @@ package com.example.stock.service;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -21,6 +22,7 @@ public class StockService {
         서버가 1대일때는 되는듯싶으나 여러대의 서버를 사용하게되면 사용하지 않았을때와 동일한 문제가 발생된다.
         인스턴스단위로 thread-safe 이 보장이 되고, 여러서버가 된다면 여러개의 인스턴스가 있는것과 동일기때문입니다.
      */
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public synchronized void decress(Long id, Long quantity) {
         /*
             get stock
